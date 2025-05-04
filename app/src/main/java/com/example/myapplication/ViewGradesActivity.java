94591fa4f15b3dd667c20e1853d99f96a1cc221b
ikhidar <ikhidar@csumb.edu>
2025-05-04 13:21:47 -0700

    public class

--- app/src/main/java/com/example/myapplication/ViewGradesActivity.java
+++ app/src/main/java/com/example/myapplication/ViewGradesActivity.java
@@ -1,4 +1,11 @@
 package com.example.myapplication;
 
-public class ViewGradesActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ViewGradesActivity extends AppCompatActivity {
+    private RecyclerView gradesRecyclerView;
+    private GradeWithAssignmentAdapter adapter;
+
+
 }
