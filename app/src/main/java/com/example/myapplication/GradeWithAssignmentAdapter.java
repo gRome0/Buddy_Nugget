f363812b6d4d2a241f9d07a26c920415894e8ea5
josep <jfedeli@gmail.com>
2025-05-03 21:04:12 -0700

    Fixed issue with this.gradeList, created skeleton for rest of code block.

--- app/src/main/java/com/example/myapplication/GradeWithAssignmentAdapter.java
+++ app/src/main/java/com/example/myapplication/GradeWithAssignmentAdapter.java
@@ -1,5 +1,7 @@
 package com.example.myapplication;
 
+import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -7,11 +9,21 @@ import java.util.List;
 
 public class GradeWithAssignmentAdapter extends RecyclerView.Adapter<GradeWithAssignmentAdapter.ViewHolder> {
     private List<GradeWithAssignment> gradeList;
-    public GradeWithAssignmentAdapter(List<GradeWithAssignment> gradeList) { this.gradeList + gradeList; }
+    public GradeWithAssignmentAdapter(List<GradeWithAssignment> gradeList) { this.gradeList = gradeList; }
+
+    @Override
+    public void onBindViewHolder(@NonNull GradeWithAssignmentAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     @NonNull
     @Override
-    public GradeWithAssignmentAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
-
+    public GradeWithAssignmentAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
     }
 }
