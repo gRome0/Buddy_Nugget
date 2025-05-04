ddc4504b6a45c2d2cda020f235069f93fba1fc65
gRome0 <gmartinezromero@csumb.edu>
2025-05-04 16:01:39 -0700

    private;s and start username

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,8 +2,53 @@ package com.example.myapplication;
 
 
 
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity{
+private EditText usernameField, passwordField;
+private Button loginButton;
+
+private AppDatabase db;
+private UserDao userDao;
+
+@Override
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
