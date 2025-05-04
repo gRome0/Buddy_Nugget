c3538cea8e0d7d2e951ae1fc2789c0a178c385ea
ikhidar <ikhidar@csumb.edu>
2025-05-03 23:33:36 -0700

    commit

--- app/src/main/java/com/example/myapplication/UserDashboardActivity.java
+++ app/src/main/java/com/example/myapplication/UserDashboardActivity.java
@@ -1,4 +1,15 @@
 package com.example.myapplication;
 
-public class UserDashboardActivity {
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserDashboardActivity extends AppCompatActivity {
+    private Button viewAssignmentBtn, viewAssignmentsBtn, viewGradesBtn, logoutBtn;
+
+    @Override
+    protected void Oncreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState);
+
+    }
 }
