22f0b93393f84c939f1bcffcda00b4db28c80032
ikhidar <ikhidar@csumb.edu>
2025-05-03 23:40:55 -0700

    commit

--- app/src/main/java/com/example/myapplication/UserDashboardActivity.java
+++ app/src/main/java/com/example/myapplication/UserDashboardActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -10,6 +11,18 @@ public class UserDashboardActivity extends AppCompatActivity {
 
     @Override
     protected void Oncreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState);
+
+        SharedPreferencesprefs = getSharedPreferences("UserPrefs", MODE_PRIVATE);
+        int userId = prefs.getInt("userId", -1);
+        if(userId == -1) { finish (); return; }
+
+        User user = AppDatabase.getInstance(getApplicationContext()).userDao().getUserById(userId);
+        if (user.getRole() != User.Role.NORMAL) { finish(); return; }
+
+        setContentView(R.layout.activity_admin_dashboard);
+        viewAssignmentBtn = findViewById(R.id.viewAssignmentsBtn);
+        viewGradesBtn = findViewById(R.id.viewGradeBtn);
+        logoutBtn = findViewById(R.id.logoutBtn);
 
     }
 }
