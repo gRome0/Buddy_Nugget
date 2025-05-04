8b91785451c48a2c38a46c3d0c28295389d73a6b
josep <jfedeli@gmail.com>
2025-05-03 21:02:39 -0700

    Creating commit before implementing any imports

--- app/src/main/java/com/example/myapplication/GradeWithAssignmentAdapter.java
+++ app/src/main/java/com/example/myapplication/GradeWithAssignmentAdapter.java
@@ -1,4 +1,17 @@
 package com.example.myapplication;
 
-public class GradeWithAssignmentAdapter {
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class GradeWithAssignmentAdapter extends RecyclerView.Adapter<GradeWithAssignmentAdapter.ViewHolder> {
+    private List<GradeWithAssignment> gradeList;
+    public GradeWithAssignmentAdapter(List<GradeWithAssignment> gradeList) { this.gradeList + gradeList; }
+
+    @NonNull
+    @Override
+    public GradeWithAssignmentAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+
+    }
 }
