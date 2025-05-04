aa9e897707bdbf665f6472eaad12539e5e78f2dc
josep <jfedeli@gmail.com>
2025-05-03 20:26:56 -0700

    Created members and constructor

--- app/src/main/java/com/example/myapplication/Grade.java
+++ app/src/main/java/com/example/myapplication/Grade.java
@@ -1,4 +1,16 @@
 package com.example.myapplication;
 
 public class Grade {
+
+    private int gradeId;
+    private int assignmentId;
+    private String studentId;
+    private float score;
+
+    public Grade(int gradeId, int assignmentId, String studentId, float score){
+        this.gradeId = gradeId;
+        this.assignmentId = assignmentId;
+        this.studentId = studentId;
+        this.score = score;
+    }
 }
