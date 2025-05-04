fd603bd1d6f923a9c17d5aa3f6448ebbe6843dac
josep <jfedeli@gmail.com>
2025-05-03 21:26:41 -0700

    Working on completing onCreate, current errors should be fixed when merged

--- app/src/main/java/com/example/myapplication/GradeAssignmentActivity.java
+++ app/src/main/java/com/example/myapplication/GradeAssignmentActivity.java
@@ -1,14 +1,28 @@
 package com.example.myapplication;
 
+import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class GradeAssignmentActivity extends AppCompatActivity {
     private Spinner assignmentSpinner;
     private EditText studentIdField, scoreField;
     private Button gradeBtn;
+    private Map<String, Integer> assignmentMap = new HashMap<>();
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grade_assignment);
+        assignmentSpinner = findViewById(R.id.gradeScore); gradeBtn = findViewById(R.id.submitGradeBtn);
+        AppDatabase db = AppDatabase.getInstance(getApplicationContext());
+        List<Assignment> assignments = db.assignmentDao().getAllAssignments();
+    }
 }
