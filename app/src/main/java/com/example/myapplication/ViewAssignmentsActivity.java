34e22cbea81b43e0410abc7b23f7e3697a81b9d9
gRome0 <gmartinezromero@csumb.edu>
2025-05-04 18:45:46 -0700

    new import added to ViewAssignmentsActivity

--- app/src/main/java/com/example/myapplication/ViewAssignmentsActivity.java
+++ app/src/main/java/com/example/myapplication/ViewAssignmentsActivity.java
@@ -3,8 +3,11 @@ package com.example.myapplication;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class ViewAssignmentsActivity extends AppCompatActivity {
 
