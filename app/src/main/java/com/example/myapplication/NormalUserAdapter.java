ac9960469d86543bee9a19b36a15e6acc3ad3ca9
ikhidar <ikhidar@csumb.edu>
2025-05-04 21:21:32 -0700

    username and id

--- app/src/main/java/com/example/myapplication/NormalUserAdapter.java
+++ app/src/main/java/com/example/myapplication/NormalUserAdapter.java
@@ -1,4 +1,23 @@
 package com.example.myapplication;
 
-public class NormalUserAdapter {
+import android.view.LayoutInflater;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class NormalUserAdapter extends RecyclerView.Adapter<NormalUserAdapter.NormalUserViewHolder> {
+    private List<User> userList;
+    public NormalUserAdapter(List<User> userList) {
+        this.userList = userList;
+    }
+    @NonNull
+
+    @Override
+    public NormalUserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user_row, parent, false);
+        return new NormalUserViewHolder(view);}
+
+    }
 }
