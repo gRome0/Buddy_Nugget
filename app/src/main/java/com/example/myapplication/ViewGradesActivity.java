f72377d35300a44fd8dc193b9c73af79bdb274de
ikhidar <ikhidar@csumb.edu>
2025-05-04 20:40:26 -0700

    onCreate

--- app/src/main/java/com/example/myapplication/ViewGradesActivity.java
+++ app/src/main/java/com/example/myapplication/ViewGradesActivity.java
@@ -1,9 +1,11 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -21,6 +23,22 @@ public class ViewGradesActivity extends AppCompatActivity {
         if (userId == -1)
         {Toast.makeText(this, "No user logged in.", Toast.LENGTH_SHORT.show()); finish();return; }
         String studentId = student.getStudentId();
+
+        db.gradeDao().getGradesWithAssignmentsLive(studentId).observe(this, new Observer<List>GradeWithAssignment>>()) {
+            @Override
+            public void onChanged(List<GradeWithAssignment> grades)
+            {
+                adapter = new GradeWithAssignmentAdapter(grades);
+                gradesRecyclerView.setAdapter(adapter);
+
+                if (grades.isEmpty()) {
+                    Toast.makeText(ViewGradesActivity.this, "No grades found.", Toast.LENGTH_SHORT);
+                }
+            });
+            }
+            public static Intent newIntent(Context context){
+            return new Intent(context, ViewGradesActivity.class);
+        }
 
 
     }
