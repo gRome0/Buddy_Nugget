08a1e24c3de0d22562762bd5e15a1fc10eaa5c0c
Javier <jgalarza@csumb.edu>
2025-05-05 01:00:54 -0700

    views for student from database

--- app/src/main/java/com/example/myapplication/StudentRosterForTeacherActivity.java
+++ app/src/main/java/com/example/myapplication/StudentRosterForTeacherActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class StudentRosterForTeacherActivity extends AppCompatActivity {
 
@@ -12,6 +15,15 @@ public class StudentRosterForTeacherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView();
+        setContentView(R.layout.activity_student_roster_for_teacher);
+
+        studentRecyclerView = findViewById(R.id.studentRecyclerView);
+        studentRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        AppDatabase db = AppDatabase.getInstance(getApplicationContext());
+        List<User> students = db.userDao().getUsersByRole(User.Role.NORMAL.name());
+
+        adapter = new NormalUserAdapter(students);
+        studentRecyclerView.setAdapter(adapter);
     }
 }
