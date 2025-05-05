bec6c66ce2470f925d02477b0562a8a8e3b8f743
gRome0 <gmartinezromero@csumb.edu>
2025-05-05 04:16:36 -0700

    Have added the createAssinBtn and logout

--- app/src/main/java/com/example/myapplication/AdminDashboardActivity.java
+++ app/src/main/java/com/example/myapplication/AdminDashboardActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -18,15 +19,11 @@ public class AdminDashboardActivity extends AppCompatActivity  {
 
         SharedPreferences prefs = getSharedPreferences("UserPrefs", MODE_PRIVATE);
         int userId = prefs.getInt("userId", -1);
-        if (userId == -1) {
-            finish();
-            return;
-        }
+        if (userId == -1) { finish(); return; }
         User user = AppDatabase.getInstance(getApplicationContext()).userDao().getUserById(userId);
-        if (user.getRole() != User.Role.ADMIN) {
-            finish();
-            return;
-        }
+
+        if (user.getRole() != User.Role.ADMIN) { finish(); return; }
+
         setContentView(R.layout.activity_admin_dashboard);
         createUserBtn = findViewById(R.id.createUserBtn);
         createAssignmentBtn = findViewById(R.id.createAssignmentBtn);
@@ -37,3 +34,23 @@ public class AdminDashboardActivity extends AppCompatActivity  {
                 startActivity(new Intent(AdminDashboardActivity.this, CreateUserActivity.class));
             }
         });
+
+        createAssignmentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {startActivity(new Intent(AdminDashboardActivity.this, CreateAssignmentActivity.class));}
+        });
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {prefs.edit().remove("userId").apply();
+                Intent intent = new Intent(AdminDashboardActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK); startActivity(intent); finish();
+            }
+        });
+    }
+
+    public static Intent newIntent(Context context)
+    {return new Intent(context, AdminDashboardActivity.class); }
+}
