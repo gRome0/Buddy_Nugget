8a24e735f7f557eac1c6280bd0067ee35984dcc7
Javier <jgalarza@csumb.edu>
2025-05-05 00:20:17 -0700

    putting on right branch

--- app/src/main/java/com/example/myapplication/TeacherDashboardActivity.java
+++ app/src/main/java/com/example/myapplication/TeacherDashboardActivity.java
@@ -1,11 +1,18 @@
 package com.example.myapplication;
-
+import android.os.Bundle;
 import android.widget.Button;
 
-public class TeacherDashboardActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TeacherDashboardActivity extends AppCompatActivity {
     private Button createAssignmentBtn;
     private Button gradeAssignmentsBtn;
     private Button viewStudentRosterBtn;
-
     private Button logoutBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_dashboard);
+    }
 }
