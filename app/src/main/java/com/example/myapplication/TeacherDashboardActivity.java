f6aec31556f79a31ddf2492828236c9da52d4533
Javier <jgalarza@csumb.edu>
2025-05-05 00:29:11 -0700

    user preferences

--- app/src/main/java/com/example/myapplication/TeacherDashboardActivity.java
+++ app/src/main/java/com/example/myapplication/TeacherDashboardActivity.java
@@ -1,4 +1,5 @@
 package com.example.myapplication;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -14,5 +15,18 @@ public class TeacherDashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_dashboard);
+
+        SharedPreferences prefs = getSharedPreferences("UserPrefs", MODE_PRIVATE);
+        int userId = prefs.getInt("userId", -1);
+        if (userId == -1) {
+            finish();
+            return;
+        }
+
+        User user = AppDatabase.getInstance(getApplicationContext()).userDao().getUserById(userId);
+        if (user.getRole() != User.Role.TEACHER) {
+            finish();
+            return;
+        }
     }
 }
