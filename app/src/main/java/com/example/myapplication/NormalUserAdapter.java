bffec2b0e40b99dc756946c30d7c66e1f5e1f2fb
ikhidar <ikhidar@csumb.edu>
2025-05-05 20:05:29 -0700

    getitemcount

--- app/src/main/java/com/example/myapplication/NormalUserAdapter.java
+++ app/src/main/java/com/example/myapplication/NormalUserAdapter.java
@@ -12,29 +12,39 @@ import java.util.List;
 
 public class NormalUserAdapter extends RecyclerView.Adapter<NormalUserAdapter.NormalUserViewHolder> {
     private List<User> userList;
+
     public NormalUserAdapter(List<User> userList) {
         this.userList = userList;
     }
+
     @NonNull
 
     @Override
-    public NormalUserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
-    {
+    public NormalUserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user_row, parent, false);
-        return new NormalUserViewHolder(view);}
+        return new NormalUserViewHolder(view);
+    }
 
     @Override
-    public void onBindViewHolder(@NonNull NormalUserViewHolder holder, int position)
-    {User user = userList.get(position); holder.usernameView.setText("Username: " + user.getUsername()); holder.studentIdView
-            ("Student ID: " + (user.getStudentId() != null ? user.getStudentId() : "N/A")); }
+    public void onBindViewHolder(@NonNull NormalUserViewHolder holder, int position) {
+        User user = userList.get(position);
+        holder.usernameView.setText("Username: " + user.getUsername());
+        holder.studentIdView
+                ("Student ID: " + (user.getStudentId() != null ? user.getStudentId() : "N/A"));
+    }
 
     @Override
     public int getItemCount() {
         return userList.size();
     }
-    public static class NormalUserViewHolder extends RecyclerView.ViewHolder
-    { TextView usernameView, studentIdView; public NormalUserViewHolder(@NonNull View itemView)
-    { super(itemView); usernameView = itemView.findViewById(R.id.usernameText); studentIdView = itemView.findViewById(R.id.student)
-    }
+
+    public static class NormalUserViewHolder extends RecyclerView.ViewHolder {
+        TextView usernameView, studentIdView;
+
+        public NormalUserViewHolder(@NonNull View itemView) {
+            super(itemView);
+            usernameView = itemView.findViewById(R.id.usernameText);
+            studentIdView = itemView.findViewById(R.id.student)
+        }
     }
 } //
