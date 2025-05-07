8f151cc88295cb1976d568ad288c511f840588b8
josep <jfedeli@gmail.com>
2025-05-06 21:48:34 -0700

    This unit tests if the grade assignment box is empty, the second one makes sure that assignment details are filled out correctly

--- app/src/androidTest/java/com/example/myapplication/GradeWithAssignmentAdapterTest.java
+++ app/src/androidTest/java/com/example/myapplication/GradeWithAssignmentAdapterTest.java
@@ -7,5 +7,20 @@ public class GradeWithAssignmentAdapterTest {
     public void makeSureItsRightAssign(){
         Grade grade = new Grade(0, 1, "STU123", 84.0f);
         Assignment assignment = new Assignment(1, "Defeat EnderDragon", "You beat the dragon game over jk", "2025-05-20");
+        GradeWithAssignment gwa = new GradeWithAssignment();
+        gwa.grade = grade;
+        gwa.assignment = assignment;
+
+        GradeWithAssignmentAdapter adapter = new GradeWithAssignmentAdapter(Collections.singletonList(gwa));
+
+        ViewGroup parent = new android.widget.FrameLayout(ApplicationProvider.getApplicationContext());
+        GradeWithAssignmentAdapter.ViewHolder holder = adapter.onCreateViewHolder(parent, 0);
+        adapter.onBindViewHolder(holder, 0);
+
+        TextView titleView = holder.assignmentTitle;
+        TextView scoreView = holder.gradeScore;
+
+        assertEquals("Assignment: Defeat EnderDragon", titleView.getText().toString());
+        assertEquals("Score: 84.0", scoreView.getText().toString());
     }
 }
